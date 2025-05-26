0b5ca129a6405997bff5e14431319ffb9d0fc4a3
Iasmim Horrana <horranaiasmim@gmail.com>
2025-05-26 10:25:35 -0300

    update-main

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 # Ponto de entrada do projeto (orquestra o que os módulos fazem)
 
 from scraping.driver import iniciar_driver
-from scraping.acoes import abrir_site, botao_moeda, selecionar_moeda_brl
+from scraping.acoes import abrir_site, botao_moeda, selecionar_moeda_brl, selecionar_300_moedas
+from scraping.coleta import coletar_dados
 
 def main():
     driver = iniciar_driver()
     abrir_site(driver)
     botao_moeda(driver)
     selecionar_moeda_brl(driver)
+    selecionar_300_moedas(driver)
+
+    # Salva o HTML da página carregada
+    with open("pagina.html", "w", encoding="utf-8") as f:
+        f.write(driver.page_source)
 
     input("Pressione Enter para fechar o navegador...")
     driver.quit()
