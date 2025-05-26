c7621dae36efd0e341ed3b5659d70e1cd8b81766
Iasmim Horrana <horranaiasmim@gmail.com>
2025-05-26 15:57:07 -0300

    update-acoes-e-coleta

--- scraping/acoes.py
+++ scraping/acoes.py
@@ -1,6 +1,10 @@
 # Funções que fazem ações no site (abrir, clicar, raspar, etc)
+import time
+import pandas as pd
+import os # Importante para manipulação de arquivos
 from selenium.webdriver.common.by import By
-from .utils import esperar_elemento  # usa a função que centraliza esperas
+from .utils import esperar_elemento  
+from .coleta import coletar_dados
 
 def abrir_site(driver, url="https://www.coingecko.com/pt"):
     """Abre o site e espera o carregamento do título"""
@@ -25,3 +29,32 @@ def selecionar_300_moedas(driver):
     opcao_300 = esperar_elemento(driver, By.CSS_SELECTOR, 'span[data-url="/pt?items=300"]')
     opcao_300.click()
 
+def botao_next(driver, total_paginas=58):
+    todos_dados = []
+
+    for i in range(1, total_paginas + 1):
+        print(f"Coletando dados da página {i}...")
+        df = coletar_dados(driver)
+        if not df.empty:
+            todos_dados.append(df)
+
+        try:
+            botao_next = esperar_elemento(driver, By.CSS_SELECTOR, 'a[aria-label="next"]')
+            botao_next.click()
+            time.sleep(3)
+        except Exception as e:
+            print(f"Erro ao clicar no botão da página {i + 1}: {e}")
+            break
+
+    # Junta todos os dados coletados
+    resultado = pd.concat(todos_dados, ignore_index=True)
+
+    # Garante que a pasta data exista
+    os.makedirs("data", exist_ok=True)
+
+    # Salvando em vários formatos
+    resultado.to_csv("data/dados_cripto.csv", index=False)
+    resultado.to_parquet("data/dados_cripto.parquet", index=False)
+    resultado.to_json("data/dados_cripto.json", orient="records", lines=True)
+
+    print("Dados coletados e exportados com sucesso.")
--- scraping/coleta.py
+++ scraping/coleta.py
@@ -5,8 +5,21 @@ from selenium.webdriver.common.by import By
 
 def coletar_dados(driver):
     html = StringIO(driver.page_source)
-    tabelas = pd.read_html(html)
-    if tabelas:
-        return tabelas[0]
-    else:
+    tabelas = pd.read_html(html, thousands=".", decimal=",", skiprows=1)
+
+    if not tabelas:
         return pd.DataFrame()
+    
+    df = tabelas[0]
+
+    # Remove linhas totalmente vazias
+    df = df.dropna(how="all").reset_index(drop=True)
+
+    # Renomeia colunas
+    df.columns = ['Favorito', 'id', 'Moeda', 'Comprar', 'Preço', '1h', '24h', '7d', '30d',
+                  'Volume 24h', 'Capitalização de Mercado', 'FDV', 'Cap/FDV', 'Grafico_7dias']
+
+    # Remove colunas desnecessárias
+    df.drop(columns=['Favorito', 'Comprar', 'Grafico_7dias'], inplace=True)
+
+    return df
